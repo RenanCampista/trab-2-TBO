315830512312c26badc8d5f243bb471a5ced1d59
RenanCampista <renan.campista@edu.ufes.br>
2024-07-23 18:30:34 -0300

    feat: atualiza o arquivo main.c com a solução
    Ainda não foi realizada a ordenação, mas o programa já é capaz de resolver o problema e desalocar completamente a memória.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "network.h"
+#include "problem.h"
 
 int main(int argc, char *argv[]) {
     // ./trab2 <path_input_file> <path_output_file>
@@ -8,8 +9,8 @@ int main(int argc, char *argv[]) {
         exit(printf("Error: invalid number of arguments.\n"));
 
     Network *network = network_read(argv[1]);
-    network_print(network);
+    problem_solve(network, argv[2]);
     network_destruct(network);
-    
+
     return 0;
 }
